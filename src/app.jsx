e292d700007067a302917653ccf350136617834c
Tanmay Khandelwal <tanmay.khandelwal@creativewebo.com>
2025-06-28 23:00:57 +0530

    implement cocktail sections

--- src/app.jsx
+++ src/app.jsx
@@ -3,16 +3,17 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-gsap.registerPlugin(ScrollTrigger,SplitText);
+import Cocktails from './components/Cocktails';
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <main>
-        <Navbar/>
-        <Hero/>
-        <div className='h-dvh bg-black'></div>
-    </main>
-  )
+    return (
+        <main>
+            <Navbar />
+            <Hero />
+            <Cocktails/>
+        </main>
+    )
 }
 
 export default App
